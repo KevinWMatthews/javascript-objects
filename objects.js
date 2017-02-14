9338fb7c21546d15a5ab37facdc5c75a63ef604a
Kevin Matthews <kmatthews@cyberdata.net>
2017-02-14 09:57:53 -0800

    Create an object with an Object Initializer

--- objects.js
+++ objects.js
@@ -19,3 +19,21 @@ function create_object() {
 function named_function() {
     console.log('I am a function with a name.');
 }
+
+function object_initializers() {
+    var myObject = {                // The Object consists of everything within braces.
+        property1: 'value1',        // Separate properties and values with a colon.
+        property2: 'value2',        // Separate different propertis with a comma.
+        function1: named_function,
+        function2: function() {
+            console.log('I too am an anonymous function');
+        }
+    }
+
+    console.log(myObject);
+    console.log(myObject.property1);
+    console.log(myObject['property2']);
+    console.log(myObject.property3);
+    myObject.function1();
+    myObject.function2();
+}
